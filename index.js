64cb2dee51be11c9b8d4c3e8e85c047a8f4c34e0
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-01-10 19:26:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key){
   var object = Object.assign({}, object { [ key ] } )
+  delete object.key 
+  return object
 }

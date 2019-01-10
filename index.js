01257bc71514e97dadd5c0d47ee2005e5efd4e3c
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-01-10 19:11:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+function Objects() {
+  return recipes
+
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(recipes, object, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object[key] = value;
+}
+
+function deleteFromObjectByKey(object, key){
+  
+
+}
